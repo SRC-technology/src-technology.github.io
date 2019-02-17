4839f14b37885449bb7684ee3b5be0e10c0f8a66
Xavier Via <fernando.via@gmail.com>
2019-02-17 13:57:43 +0100

    Have something for the index

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
+import { Fragment } from 'react'
 import Viewport from '../src/Viewport'
 import Lattice from '../src/Lattice'
+import Dots from '../src/Dots'
+import Logo from '../src/Logo'
 
 export default () => (
   <Viewport>
     {({ width, height, devicePixelRatio }) => (
-      <Lattice 
-        width={width}
-        height={height}
-        devicePixelRatio={devicePixelRatio}
-      />
+      <Fragment>
+        <Dots
+          width={width}
+          height={height}
+          devicePixelRatio={devicePixelRatio}
+        />
+        <Lattice 
+          width={width}
+          height={height}
+          devicePixelRatio={devicePixelRatio}
+        />
+        <div style={{zIndex: 20}}>
+          <Logo 
+            width={width}
+            height={height}
+            devicePixelRatio={devicePixelRatio}
+          />
+        </div>
+      </Fragment>
     )}
   </Viewport>    
 )
